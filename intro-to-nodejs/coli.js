3a5aef8f5e9b3f6798b59222c812c96492c2d348
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-03-28 15:25:47 +0000

    Add utils folder.

--- intro-to-nodejs/coli.js
+++ intro-to-nodejs/coli.js
@@ -1,5 +1,18 @@
 #!/usr/bin/env node
+// This script demonstrates how to create a simple command-line application using Node.js.
 
+import {count} from './utils/utils.js'; // ES6 module syntax
+import whatever from './utils/utils.js';
+// console.log(count(5)); // 5  
+// console.log(whatever.name); // cookies
+
+import fs from 'node: fs';
+import _ from 'lodash'; // lodash is a utility library for JavaScript
+
+// console.log(process.argv); // ['node', 'script.js', 'arg1', 'arg2']
+
+const {count} = require('./utils/utils.js'); // CommonJS syntax
+ 
 const note = process.argv[2];
 const newNote = {
   content: note,
---
+++ intro-to-nodejs/utils/utils.js
@@ -0,0 +1,7 @@
+export const count = num => num; // ES6 module syntax
+exports.count = num => num;
+
+module.exports = {count}; // CommonJS syntax
+
+export default {name: 'cookies'} // ES6 module syntax
+
