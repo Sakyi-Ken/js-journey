ebcc2fc29c29091b1bc380b0988b6813c8d2f2df
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-03-30 23:27:28 +0000

    Remove parameter from the showGames function.

--- Js-Intro/solution.js
+++ Js-Intro/solution.js
@@ -14,7 +14,7 @@ class Console {
   addGame(game) {
     this.games.push(game);
   }
-  showGames(cd) {
+  showGames() {
     console.log("List of games coming soon!");
   }
 }
@@ -27,5 +27,5 @@ myConsole.addGame(new Game('Super Racing', 'Racing'));
 console.log(myConsole.games); // [Game { title: 'Space Adventure', genre: 'Action' }, Game { title: 'Super Racing', genre: 'Racing' }]
 // console.log(myConsole.games[0].title); // Space Adventure
 
-console.log(myConsole.games[0]); // Space Adventure
+console.log(myConsole.games[0]); //{title: 'Space Adventure', genre: 'Action'} 
 console.log(myConsole.games[1].title); // Super Racing
