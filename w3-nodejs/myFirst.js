d4301016f42e17c47904eb0bb7934cc158ab3b4a
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-03-31 02:21:45 +0000

    Make use of req and its method

--- w3-nodejs/myFirst.js
+++ w3-nodejs/myFirst.js
@@ -2,9 +2,15 @@
 
 import {createServer} from 'http';
 import {myDateTime} from './myfirstModule.js';
+//import {url} from 'url';
+//var url = require('url');
 
 createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('The date and time are currently: ' + myDateTime());
-  res.end();
+  res.write(req.url);
+  res.write(req.method);
+  // var q = url.parse(req.url, true).query;
+  // var txt = q.year + " " + q.month;
+  //res.end(txt);
 }).listen(8080);
