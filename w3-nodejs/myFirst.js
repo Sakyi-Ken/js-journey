2d72dd9f65eacce3079f6af37253ff9bf5369639
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-03-31 00:53:47 +0000

    Import the module in this file.

--- w3-nodejs/myFirst.js
+++ w3-nodejs/myFirst.js
@@ -1,8 +1,10 @@
 //var http = require('http');
 
 import {createServer} from 'http';
+import {myDateTime} from './myfirstModule.js';
 
 createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World! Hi there');
+  res.write('The date and time are currently: ' + myDateTime());
+  res.end();
 }).listen(8080);
